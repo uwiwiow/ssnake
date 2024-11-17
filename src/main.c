4c2fef5ddbcf9bf19e603e6c100d08da18e96dcd
uwiwiow <uwiwiow@gmail.com>
2024-11-17 14:10:34 -0600

    add automatic movement

--- src/main.c
+++ src/main.c
@@ -9,10 +9,13 @@ void input(Vector2 *d);
 void move(Vector2 *snake, Vector2 d);
 Vector2 randVec2ScreenPos();
 void procmove(Vector2 *curr, Vector2 prev, float dist);
+double getAngleRelSDF(Vector2 s, Vector2 d, Vector2 f);
+void botmove(double a, Vector2 *d);
 
 const int width = 640;
 const int height = 480;
 float body = 10.0f;
+bool botmode = false;
 
 int main () {
 
@@ -28,6 +31,11 @@ int main () {
     while(!WindowShouldClose()) {
 
         input(&d);
+
+        if (botmode) {
+            botmove(getAngleRelSDF(snakes[0], d, food), &d);
+        }
+
         move(&snakes[0], d);
 
         if (CheckCollisionCircles(snakes[0], body, food, body/2)) {
@@ -50,8 +58,13 @@ int main () {
 
             DrawCircleV(food, body/2, RED);
 
+            if (botmode) {
+                DrawLineV(snakes[0], Vector2Add(snakes[0], Vector2Scale(d, 600.0f)), BLUE);
+                DrawText(TextFormat("%.2lf", getAngleRelSDF(snakes[0], d, food)), 150, 10, 20, WHITE);
+            }
 
-            DrawFPS(10, 10);
+            DrawText(TextFormat("Controls:\nA - S: move\nB: botmode"), 10, 10, 20, WHITE);
+            DrawFPS(10, height - 30);
 
         EndDrawing();
     }
@@ -62,22 +75,24 @@ int main () {
 }
 
 void input(Vector2 *d) {
-    if (IsKeyDown(KEY_A)) {
+    if (IsKeyDown(KEY_A))
         *d = Vector2Rotate(*d, -5 * GetFrameTime());
-    }
-    if (IsKeyDown(KEY_D)) {
+
+    if (IsKeyDown(KEY_D))
         *d = Vector2Rotate(*d, 5 * GetFrameTime());
-    }
+
+    if (IsKeyPressed(KEY_B))
+        botmode = !botmode;
 }
 
 void move(Vector2 *snake, Vector2 d) {
-    snake->x = snake->x + (d.x * 100 * GetFrameTime());
-    snake->y = snake->y + (d.y * 100 * GetFrameTime());
+    snake->x = snake->x + (d.x * (100 + body) * GetFrameTime());
+    snake->y = snake->y + (d.y * (100 + body) * GetFrameTime());
 }
 
 Vector2 randVec2ScreenPos() {
-    return (Vector2) {(float) GetRandomValue(20, 620),
-                      (float) GetRandomValue(20, 460)};
+    return (Vector2) {(float) GetRandomValue(20, width - 20),
+                      (float) GetRandomValue(20, height - 20)};
 }
 
 void procmove(Vector2 *curr, Vector2 prev, float dist) {
@@ -89,4 +104,28 @@ void procmove(Vector2 *curr, Vector2 prev, float dist) {
     }
     *curr = Vector2Add(prev, direction);
 
+}
+
+double getAngleRelSDF(Vector2 s, Vector2 d, Vector2 f) {
+    Vector2 v = Vector2Subtract(f, s);
+
+    double magnitude_v = Vector2Length(v);
+    if (magnitude_v == 0) {
+        return 0;
+    }
+
+    double angle = acos(Vector2DotProduct(d, v) / magnitude_v);
+
+    if ((d.x * v.y - d.y * v.x) < 0) {
+        angle = -angle;
+    }
+
+    return angle * RAD2DEG;
+}
+
+void botmove(double a, Vector2 *d) {
+    if (a < -1.0f)
+        *d = Vector2Rotate(*d, -5 * GetFrameTime());
+    if (a > 1.0f)
+        *d = Vector2Rotate(*d, 5 * GetFrameTime());
 }
